8fbf79707b38e4e98b524b963d55110bacfb90c8
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-03-18 12:38:31 +0800

    Solve A, now working on B

--- LeetCode Weekly Contest/Weekly_Contest_76/PB.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_76/PB.cpp
@@ -3,23 +3,8 @@ class Solution {
 public:
     int minSwap(vector<int>& A, vector<int>& B)
     {
-        int aend,bend
-        FORI(A.size()-1)
-        {
-            if(A[i]>=A[i+1])
-            {
-                aend = i+1;
-            }
-        }
-        FORI(B.size()-1)
-        {
-            if(B[i]>=B[i+1])
-            {
-                bend = i+1;
-            }
-        }
-        int minst = min(aend,bend);
-        for(i=minst;i<A.size();i++)
+        int sz = A.size();
+        FORI(sz)
         {
             
         }
--- LeetCode Weekly Contest/Weekly_Contest_76/PD.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_76/PD.cpp
@@ -1,46 +1,8 @@
-class Solution {
+class Solution
+{
 public:
-    int preimageSizeFZF(int kin)
+    vector<int> hitBricks(vector<vector<int>>& grid, vector<vector<int>>& hits)
     {
-        int cnt = 0, factorial = 0, zeros = 0, current_pow = 25;
-        while(1)
-        {
-            if(factorial ==  current_pow * 5)
-            {
-                cout<<" factorial is now "<<factorial<<" current_pow is now "<<current_pow<<endl;
-                current_pow *= 5;
-            }
-            if(factorial % current_pow == 0 && factorial)
-            {
-                zeros += ((int)(log(factorial) / log(5)));
-                cout<<" factorial is of 25 multiple "<<factorial<<" zeros "<<zeros <<" add "<<((int)(log(factorial) / log(5)))<<endl;
-            }
-            else if(factorial % 5 ==0 && factorial)
-            {
-                cout<<" factorial not 25 "<<factorial<<endl;
-                zeros++;
-            }
 
-            if(zeros == kin)
-            {
-                cnt++;
-            }
-            else if(zeros > kin)
-            {
-                break;
-            }
-            cout<<" factorial "<<factorial<<" zeros "<<zeros<<endl;
-            factorial++;
-        }
-        return cnt;
-    }
-    //compare 2 casting, one is the original integer value of log and the other is the double type log, if they are equal of each other
-    //in double precision
-    //then this is correct one
-    bool is_powoffive(int real, int base)
-    {
-        int integer = (int)(log(real)/log(base));
-        double precised = (log(real)/log(base));
-        return integer == precised;
     }
 };
