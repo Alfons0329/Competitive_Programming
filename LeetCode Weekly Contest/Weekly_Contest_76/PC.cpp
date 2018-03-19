c0a79006bf2e12c37c33676a567f8ac728a2eb21
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-03-19 10:43:04 +0800

    Solve A B , working on C

--- LeetCode Weekly Contest/Weekly_Contest_76/PC.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_76/PC.cpp
@@ -12,6 +12,8 @@ public:
         {
             form_cycle.push_back(0);
         }
+        visited.resize(graph.size());
+        res.resize(graph.size());
         FORI(graph.size())
         {
             if(graph[i].size()) //if this node is not the terminal node
@@ -19,52 +21,43 @@ public:
                 //going to its next node
                 for(int i = 0 ; i < graph.size() ; i++)
                 {
-                    visited.push_back(0);
+                    visited[i] = 0;
                 }
                 cycle_cnt = 0;
                 printf("Start from:  %d \n",i);
                 dfs(i, i, graph, 0); //directly go to its next node
                 if(cycle_cnt == 0)
                 {
-                    res.push_back(i);
+                    res[i] = 1; //able to reserve
                 } //this node gives no circle, that traverse back to itself satisfie the requirements
             }
             else
             {
-                res.push_back(i);//directly push back it since such node is a terminal
+                res[i] = 1;//directly push back it since such node is a terminal
             }
         }
         //EVERY NODE THAT CONNECTS TO THE NODE THAT PROBABLY FORM A CYCLE SHOULD ALSO BE ELIMINATED
-        cout<<"Form cycle : ";
-        FORI(form_cycle.size())
-        {
-            cout<<form_cycle[i]<<" ";
-        }
-        cout<<endl;
+        //DFS AGAIN TO FIND THE NODES THAT CONNECTS TO PART OF THE CYCLE (OR SAY MAY CRASH INTO CYCLE)
         FORI(graph.size())
         {
-            for(int j = 0 ; j < graph[i].size() ; j++)
+            if(graph[i].size()) //if this node is not the terminal node
             {
-                for(int k = 0 ; k < form_cycle.size() ; k++)
+                printf("second Start from:  %d \n",i);
+                for(int i = 0 ; i < graph.size() ; i++)
                 {
-                    cout<<" i "<<i<<" j "<<j<<" k "<<k<<endl;
-                    if(graph[i][j] == k && form_cycle[k])
-                    {
-                        cout<<"ELIMINATE "<<k<<endl;
-                        res[i] = -1;
-                        break;
-                    }
+                    visited[i] = 0;
                 }
+                dfs2_eliminate(i, i, graph, 0); //directly go to its next node
             }
-
         }
         vector<int> final_res;
+        cout<<"Result size "<<res.size()<<endl;
         FORI(res.size())
         {
             cout<<res[i]<<endl;
             if(res[i] != -1)
             {
-                final_res.push_back(res[i]);
+                final_res.push_back(i);
             }
         }
         return final_res;
@@ -92,15 +85,54 @@ public:
                 break;
             }
         }
-        /*if(graph[cur_node].size() == 1 && graph[cur_node][0] == cur_node)//self cycle
-        {
-
-            cycle_cnt++;
-        }*/
         if(check_next_is_start_cycle)//a cycle formed
         {
             printf(" A cycle formed!!!! %d \n",cur_node);
             cycle_cnt++;
+        }
+        else
+        {
+            FORI(graph[cur_node].size()) //search the next node that can be traversed from the current node
+            {
+                if(!visited[graph[cur_node][i]])
+                {
+                    dfs(graph[cur_node][i], start, graph, step);
+                }
+            }
+        }
+        visited[cur_node] = false; //reach end and back track to find the next
+        cur_node = traversed_path.top();
+        cout<<"Back trace to "<<cur_node<<endl;
+        traversed_path.pop();
+    }
+    void dfs2_eliminate(int cur_node, int start, vector<vector<int>> graph, int step)
+    {
+        int connect_to_circle = 0;
+        for(int i = 0 ; i < graph[cur_node].size() ; i++)
+        {
+            for(int j = 0;j < form_cycle.size(); j++)
+            {
+                cout<<"graph "<<cur_node<<" connect to "<<graph[cur_node][i]<<"  form cycle  "<<form_cycle[j]<<endl;
+                if(graph[cur_node][i] == j && form_cycle[j] == 1) //a given node is connected to a part of the cycle
+                {
+                    printf(" A node connect to the cycle formed!!!! %d \n",cur_node);
+                    res[cur_node] = -1;
+                    connect_to_circle = 1;
+                    return;
+                }
+
+            }
+            if(connect_to_circle)
+                break;
+        }
+
+        traversed_path.push(cur_node);
+        visited[cur_node] = 1;
+        step++;
+        if(connect_to_circle && step > 1)//a cycle formed
+        {
+            res[cur_node] = -1;
+            return;
         }
         else
         {
