2157ab891ffa4629daccccd82ccea8085a11d4ee
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-03-26 00:51:28 +0800

    Finished WC67 PA PB now working on PC

--- LeetCode Weekly Contest/Weekly_Contest_67/PB.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_67/PB.cpp
@@ -27,7 +27,6 @@ public:
         {
             /* code */
             vec[str[i]-'a'].alpha = str[i];
-            cout<<"cast val "<<(int) str[i]-'a'<<endl;
             if(vec[str[i]-'a'].start == -1)
             {
                 vec[str[i]-'a'].start = i;
@@ -36,29 +35,32 @@ public:
             {
                  vec[str[i]-'a'].end = i;
             }
-            cout<<"Char "<<str[i]<<" From" <<vec[str[i]-'a'].start <<" to "<<vec[str[i]-'a'].end<<endl;
         }
         int max_start = vec[str[0]-'a'].start;
         int max_end = vec[str[0]-'a'].end;
         //greedy approach
         int partition = 0;
         vector<int>res;
-        for (size_t i = 1; i < count; i++)
+        for (size_t i = 0; i < count; i++)
         {
-            cout<<" From" <<max_start <<" to "<<max_end<<endl;
             if( vec[str[i]-'a'].start > max_start
             && vec[str[i]-'a'].start < max_end
             && vec[str[i]-'a'].end > max_end )
             {
                 max_end = vec[str[i]-'a'].end;
             }
-            if( i == max_end)
+
+            if(i == max_end || i == count - 1)//last will definitely cut over to match. just in case
             {
-                res.push_back(max_end - max_start + 1);
                 if(i != count - 1)
                 {
+                    res.push_back(max_end - max_start + 1);
                     max_start = vec[str[i + 1]-'a'].start;
                     max_end = vec[str[i + 1]-'a'].end;
+                }
+                else
+                {
+                    res.push_back(max_end - max_start + 1); // a special case
                 }
             }
         }
--- LeetCode Weekly Contest/Weekly_Contest_67/PC.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_67/PC.cpp
@@ -1,7 +1,38 @@
-#define MAXN 1010000
-#define LL long long
-#define ll __int64
-#define INF 0xfffffff
-#define mem(x) memset(x,0,sizeof(x))
-#define PI acos(-1)
-#define eps 1e-8
+class Solution
+{
+public:
+    int orderOfLargestPlusSign(int MAXN, vector<vector<int>>& mines)
+    {
+        int order = 0, count = mines.size();
+        vector<vector<int>>dp_up, dp_down, dp_left, dp_right;
+        //initialize
+        for (size_t i = 0; i < count; i++)
+        {
+            fill(dp_up[i].begin(), dp_up[i].end(), 1);
+            fill(dp_down[i].begin(), dp_down[i].end(), 1);
+            fill(dp_left[i].begin(), dp_left[i].end(), 1);
+            fill(dp_right[i].begin(), dp_right[i].end(), 1);
+        }
+        for (size_t i = 0; i < count; i++)
+        {
+            dp_up[mines[i][0]][mines[i][1]] = 0;
+            dp_down[mines[i][0]][mines[i][1]] = 0;
+            dp_left[mines[i][0]][mines[i][1]] = 0;
+            dp_right[mines[i][0]][mines[i][1]] = 0;
+        }
+
+        //main dynamic programming
+        //up continuous (bottom up)
+        for(int i = 0;i < MAXN; i++)
+        {
+            for(int j = MAXN - 2;j >= 0; j--)
+            {
+
+            }
+        }
+        //down continuous (top down)
+        //left continuous
+        //right continuous
+    }
+
+};
