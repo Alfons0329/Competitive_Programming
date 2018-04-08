97dd17e7230bca2882885f84a769897b1ff917f1
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-04-08 12:31:16 +0800

    LeetCode WC79 PA PB AC

--- LeetCode Weekly Contest/Weekly_Contest_79/PB.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_79/PB.cpp
@@ -22,42 +22,30 @@ public:
     {
         if(cur == NULL)
         {
-            cout<<"Case 1 "<<endl;
             return 0;
         }
         cout<<"Traverse to "<<cur->val<<endl;
 
         if(cur->left == NULL && cur->right == NULL && cur->val == 0)
         {
-            cout<<"Case 2 "<<endl;
             return 1;
         }
 
         if(cur->left == NULL && cur->right  && pruneit(cur->right) && cur->val == 0)
         {
-            cout<<"Case 3 "<<endl;
             return 1;
         }
 
         if(cur->left && cur->right == NULL && pruneit(cur->left) && cur->val == 0)
         {
-            cout<<"Case 4 "<<endl;
             return 1;
         }
 
         if(cur->left == NULL && cur->right == NULL && cur->val == 1)
         {
-            cout<<"Case 5 "<<endl;
             return 0;
         }
 
-        /*if(pruneit(cur->left) && pruneit(cur->right)) //if both subtree all contains 0 remove such node
-        {
-            cout<<"Case 4 "<<endl;
-            cur->left = NULL;
-            cur->right = NULL;
-            //return 1;
-        }*/
         bool left_check = pruneit(cur->left);
         bool right_check = pruneit(cur->right);
 
@@ -65,35 +53,14 @@ public:
         {
             cout<<"Case 6 "<<endl;
             cur->left = NULL;
-            // return 1;
         }
         if(right_check) //if right subtree needs to be pruned
         {
             cout<<"Case 7 "<<endl;
             cur->right = NULL;
-            // return 1;
         }
 
         return left_check & right_check & (cur->val == 0); //subtree only delete for this one
-        /*
-        if(cur->left && cur->left->val == 1)
-        {
-            return 0;
-        }
-        if(cur->left && cur->left->val == 0)
-        {
-            return 1;
-        }
-
-
-        if(cur->right && cur->right->val == 0)
-        {
-            return 0;
-        }
-        if(cur->right && cur->right->val == 1)
-        {
-            return 1;
-        }*/
 
     }
 };
