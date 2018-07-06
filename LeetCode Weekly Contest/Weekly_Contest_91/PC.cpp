9e6db000878e0e13aef0ffc1635fe028cb30ba6b
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-07-06 16:22:01 +0800

    Working on Wc91 PC

---
+++ LeetCode Weekly Contest/Weekly_Contest_91/PC.cpp
@@ -0,0 +1,19 @@
+class Solution
+{
+public:
+    struct
+    {
+
+    };
+    int matrixScore(vector<vector<int>>& arr)
+    {
+        int flag = 0;
+        while(flag == 0)
+        {
+            for(int i = 0;i < arr.size(); i++)
+            {
+                
+            }
+        }
+    }
+};
