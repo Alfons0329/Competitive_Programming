7206dde02144ca426b9ece86c241bb00a514ec6c
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-07-14 11:40:27 +0800

    Working on prob #609

--- LeetCode_OJ_Single/String/609. Find Duplicate File in System.cpp
+++ LeetCode_OJ_Single/String/609. Find Duplicate File in System.cpp
@@ -74,7 +74,7 @@ public:
     }
 
 };
-int  main(int argc, char const *argv[])
+int main(int argc, char const *argv[])
 {
     vector<string>inputFileQuery={"root/a 1.txt(abcd) 2.txt(efgh)", "root/c 3.txt(abcd)", "root/c/d 4.txt(efgh)", "root 4.txt(efgh)"};
     Solution sol;
