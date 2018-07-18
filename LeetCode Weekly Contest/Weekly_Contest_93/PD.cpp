b4a72dd60ea7ace0974c917579b5314a77db2d41
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2018-07-18 17:46:58 +0800

    Update PD.cpp

--- LeetCode Weekly Contest/Weekly_Contest_93/PD.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_93/PD.cpp
@@ -17,7 +17,7 @@ public:
         int closest_next_pos = 0, closest_next_dist = INT_MAX;
         while (1)
         {
-            if(car_pos + cur_fuel == target || car_pos == target) //should have the highest priority
+            if(car_pos + cur_fuel >= target || car_pos >= target) //should have the highest priority
             {
                 printf("reach target!\n");
                 break;
@@ -33,9 +33,9 @@ public:
                     closest_next_pos = i;
                 }
             }
-            printf("car_pos %d closest_next_pos is %d with dist %d\n",car_pos,  closest_next_pos, closest_next_dist);
+            printf("car_pos %d fuel_now closest_next_pos is %d with dist %d\n",car_pos,  closest_next_pos, closest_next_dist);
 
-            if(stations[closest_next_pos][0] - car_pos > cur_fuel)//unracehable with current fuel
+            if(stations[closest_next_pos][0] - car_pos > cur_fuel || closest_next_dist == INT_MAX)//unracehable with current fuel
             {
                 printf("unreachable to all\n");
                 return -1;
@@ -44,19 +44,20 @@ public:
             max_refuel = stations_pos = 0;
             for(int i = 0; i < stations.size(); i++) //find all reachable stations with max fuel to refill
             {
-                if(stations[i][0] - car_pos > 0 && cur_fuel > stations[i][0] - car_pos/*reachable with current fuel*/&& stations[i][1] > max_refuel /*bigger fuel slot*/)
+                printf("i %d i0 %d i1 %d ok \n", i, stations[i][0], stations[i][1]);
+                if(stations[i][0] - car_pos >= 0 && cur_fuel >= stations[i][0] - car_pos/*reachable with current fuel*/&& stations[i][1] > max_refuel /*bigger fuel slot*/)
                 {
                     max_refuel = stations[i][1];
                     stations_pos = i;
                 }
             }
-            car_pos = stations[stations_pos][0];
-            cur_fuel -= stations[stations_pos][0];
-            cur_fuel += stations[stations_pos][1];
+            cur_fuel -= (stations[stations_pos][0] - car_pos); //drive
+            car_pos = stations[stations_pos][0]; //reach
+            cur_fuel += stations[stations_pos][1]; //refill
             // stations[stations_pos][0] = INT_MAX; //marked such station as used to prevent duplicate using
             stations[stations_pos][1] = 0; //used out such fuel
             refuel++;
-            printf("select stations_pos %d, car_pos is now %d, car_fuel is now %d \n",stations_pos,  car_pos, cur_fuel);
+            printf("select stations_pos %d, car_pos is now %d, car_fuel is now %d \n",stations_pos, car_pos, cur_fuel);
         }
         return refuel;
     }
