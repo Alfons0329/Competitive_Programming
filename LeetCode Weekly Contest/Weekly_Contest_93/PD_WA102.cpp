9f451f9d88caf6a2bcbd276b6000a7e9aada1fc9
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2018-07-19 13:34:17 +0800

    AC with priority queue

--- LeetCode Weekly Contest/Weekly_Contest_93/PD_WA102.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_93/PD_WA102.cpp
@@ -1,64 +1,63 @@
+/*
+AC version, try using the priority queue. we keep traversing through the stations without refuling, that means try our best with current fuel.
+Once we traverse to a gas station with the negative fuel,
+we refill the tank with the gas station we have visited before(pushed in to the traversed_stations priority queue)
+to check if we can reach even further if we refueled before using the retroactive method.
+----------------------------------------------------------------------------------------------------------------
+Please try the following testcases for understanding how the code runs
+1000
+299
+[[13,21],[26,115],[100,47],[225,99],[299,141],[444,198],[608,190],[636,157],[647,255],[841,123]]
+*/
 class Solution
 {
 public:
+    struct mycompare
+    {
+        bool operator()(pair<int ,int>&p1, pair<int ,int>&p2) //pair first for station position and second for how much gas does a staion hace
+        {
+            return p1.second < p2.second; //sort descending to get the largest element first
+        }
+    };
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations)
     {
-        if(startFuel >= target)
-        {
-            return 0;
-        }
-        else if(stations.size() == 0)
+        if(stations.size() == 0)
         {
             return (startFuel >= target) ? 0 : -1;
         }
-        int dp[500][500] = {0}, cur_fuel = startFuel, car_pos = 0, refuel = 0;
-
-        int best_choice = 0, max_refuel = 0, stations_pos = 0, can_reach_next = 1;
-        int closest_next_pos = 0, closest_next_dist = INT_MAX;
-        while (1)
+        priority_queue<pair<int ,int>, vector<pair<int ,int>>, mycompare> traversed_stations; //logN query
+        int cur_fuel = startFuel, car_pos = 0, refuel = 0;
+        for(int i = 0; i < stations.size(); i++)
         {
-            printf("car_pos %d fuel_now %d \n",car_pos, cur_fuel);
-            if(car_pos + cur_fuel >= target || car_pos >= target) //should have the highest priority
+            cur_fuel -= (stations[i][0] - car_pos);
+            if(car_pos + cur_fuel >= target)
             {
-                printf("reach target!\n");
                 break;
             }
-            can_reach_next = 0;//search the next, closest stations to go, reachable or not (check the requirement of break if unreachable)
-            closest_next_pos = 0;
-            closest_next_dist = INT_MAX;
-            for(int i = 0; i < stations.size(); i++)
+            while(!traversed_stations.empty() && cur_fuel < 0) //traverse to certain gas station but we ran out of fuel, that we have to trace back to see if we can refuel before
             {
-                if(abs(stations[i][0] - car_pos) < closest_next_dist && stations[i][1] > 0 /*still useable*/)
-                {
-                    closest_next_dist = stations[i][0];
-                    closest_next_pos = i;
-                }
+                cur_fuel += traversed_stations.top().second;
+                refuel++;
+                traversed_stations.pop();
             }
-
-            if(stations[closest_next_pos][0] - car_pos > cur_fuel || closest_next_dist == INT_MAX)//unracehable with current fuel
+            if(cur_fuel < 0)
             {
-                printf("unreachable to all\n");
                 return -1;
             }
-
-            max_refuel = stations_pos = 0;
-            for(int i = 0; i < stations.size(); i++) //find all reachable stations with max fuel to refill
-            {
-                printf("i %d i0 %d i1 %d ok \n", i, stations[i][0], stations[i][1]);
-                if(/*stations[i][0] - car_pos >= 0*/cur_fuel >= abs(stations[i][0] - car_pos)/*reachable with current fuel*/&& stations[i][1] > max_refuel /*bigger fuel slot*/)
-                {
-                    max_refuel = stations[i][1];
-                    stations_pos = i;
-                }
-            }
-            cur_fuel -= (stations[stations_pos][0] - car_pos); //drive
-            car_pos = stations[stations_pos][0]; //reach
-            cur_fuel += stations[stations_pos][1]; //refill
-            // stations[stations_pos][0] = INT_MAX; //marked such station as used to prevent duplicate using
-            stations[stations_pos][1] = 0; //used out such fuel
-            refuel++;
-            printf("select stations_pos %d, car_pos is now %d, car_fuel is now %d \n",stations_pos, car_pos, cur_fuel);
+            car_pos = stations[i][0];
+            traversed_stations.push(make_pair(stations[i][0], stations[i][1]));;
+            has_chance_to_refuel = 0;
         }
-        return refuel;
+        //if hasn't reach the target yet
+        while(!traversed_stations.empty() && car_pos + cur_fuel < target) //traverse to certain gas station but we ran out of fuel, that we have to trace back to see if we can refuel before
+        {
+            cur_fuel += traversed_stations.top().second;
+            // car_pos = traversed_stations.top().first;
+            refuel++;
+            traversed_stations.pop();
+        }
+
+        return (car_pos + cur_fuel >= target) ? refuel : -1; //able to reach the target destination or not
     }
+
 };
