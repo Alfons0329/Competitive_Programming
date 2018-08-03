09365a81d743ebc78725a751f76c30817c54fc15
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2018-08-03 20:49:20 +0800

    AC'd PD of WC95 with Dynamic Programming

--- LeetCode Weekly Contest/Weekly_Contest_95/PD.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_95/PD.cpp
@@ -1,4 +1,4 @@
-#define ULL unsigned long long
+#define LL long long
 class Solution
 {
 public:
@@ -6,7 +6,7 @@ public:
     int profitableSchemes(int G, int P, vector<int>& group, vector<int>& profit)
     {
         int sz = group.size();
-        int dp[sz + 1][P + 1][G + 1]; //use profit for DP, 1 or padding
+        LL dp[sz + 1][P + 1][G + 1]; //use profit for DP, 1 or padding
         memset(dp, 0, sizeof(dp));
         /* Dynamic programming
         dp[i][j][k] = # of schemes has profit j with first i tasks and k people.
@@ -24,44 +24,43 @@ public:
 
         */
         dp[0][0][0] = 1; //doing nothing is 1
-        int cur_k, cur_p;
-        for(int i = 1; i <= sz; i++)
+        LL cur_k, cur_p;
+        for(LL i = 1; i <= sz; i++)
         {
             cur_k = group[i - 1];
             cur_p = profit[i - 1];
-            for(int j = 0; j < P + 1; j++)
+            for(LL j = 0; j < P + 1; j++)
             {
-                for(int k = 0; k < G + 1; k++)
+                for(LL k = 0; k < G + 1; k++)
                 {
+                    //printf(" dp task %lld, target profit%lld, current labour %lld value %lld \n", i, j, k, dp[i][j][k]);
                     if(k - cur_k < 0)
                     {
-                        printf("type 1, not enough labour ");
+                        // printf("type 1, not enough labour ");
                         dp[i][j][k] = dp[i - 1][j][k] % MOD; //currrent mission cannot be done, use the last mission
                     }
                     else if(j - cur_p < 0)
                     {
-                        printf("type 2, over profit ");
-                        dp[i][j][k] = dp[i - 1][0][k - cur_k] % MOD;
+                        // printf("type 2, over profit ");
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][0][k - cur_k]) % MOD;
                     }
                     else
                     {
-                        printf("type 3, just right ");
+                        // printf("type 3, just right ");
                         dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - cur_p][k - cur_k]) % MOD;
                     }
-                    printf(" dp task %d, target profit%d, current labour %d value %d \n", i, j, k, dp[i][j][k]);
                 }
             }
             // printf("task %d \n", i);
         }
-        int ans = 0;
+        LL ans = 0;
         
         for(int k = 0; k < G + 1; k++)
         {
-            ans += (dp[sz][P][k] % MOD);
-            // printf("ans is now %d \n", ans);
+            ans += dp[sz][P][k];
         }
         
-        return ans;
+        return ans % MOD;
 
     }
     
