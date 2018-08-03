d9fdfc26f16f3a86c23375d41fd940995dd7c2ff
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2018-08-03 20:23:43 +0800

    Update PD.cpp

--- LeetCode Weekly Contest/Weekly_Contest_95/PD.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_95/PD.cpp
@@ -6,7 +6,8 @@ public:
     int profitableSchemes(int G, int P, vector<int>& group, vector<int>& profit)
     {
         int sz = group.size();
-        ULL dp[sz][P + 1][G + 1] = {0}; //use profit for DP, 1 or padding
+        int dp[sz + 1][P + 1][G + 1]; //use profit for DP, 1 or padding
+        memset(dp, 0, sizeof(dp));
         /* Dynamic programming
         dp[i][j][k] = # of schemes has profit j with first i tasks and k people.
 
@@ -16,7 +17,7 @@ public:
         cur_k = group[i] = people needed for current mission
         cur_p = profit[i] = current profit
         dp[i][j][k] = dp[i - 1][j][k] do nothing of the current mission
-                    + dp[i][j - cur_k][k - cur_p] do this mission, but the schemes will according to the previos stored value
+                    + dp[i][j - cur_p][k - cur_k] do this mission, but the schemes will according to the previos stored value
                     i.e. how much schemes to achieve such condition with the same first i missions but only use j - cur_k and k - cur_p
                     people, also if k - cur_k < 0 then such mission cannot be done since run out of human resources
 
@@ -24,34 +25,44 @@ public:
         */
         dp[0][0][0] = 1; //doing nothing is 1
         int cur_k, cur_p;
-        for(int i = 1; i < group.size(); i++)
+        for(int i = 1; i <= sz; i++)
         {
-            cur_k = group[i];
-            cur_p = profit[i];
-            for(int j = 1; j < P + 1; j++)
+            cur_k = group[i - 1];
+            cur_p = profit[i - 1];
+            for(int j = 0; j < P + 1; j++)
             {
-                for(int k = 1; k < G + 1; k++)
+                for(int k = 0; k < G + 1; k++)
                 {
                     if(k - cur_k < 0)
                     {
-                        dp[i][j][k] = dp[i - 1][j][k]; //currrent mission cannot be done, use the last mission
+                        printf("type 1, not enough labour ");
+                        dp[i][j][k] = dp[i - 1][j][k] % MOD; //currrent mission cannot be done, use the last mission
+                    }
+                    else if(j - cur_p < 0)
+                    {
+                        printf("type 2, over profit ");
+                        dp[i][j][k] = dp[i - 1][0][k - cur_k] % MOD;
                     }
                     else
                     {
-                        dp[i][j][k] = dp[i - 1][j][k] + dp[i][j - cur_k][k - cur_p];
+                        printf("type 3, just right ");
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - cur_p][k - cur_k]) % MOD;
                     }
+                    printf(" dp task %d, target profit%d, current labour %d value %d \n", i, j, k, dp[i][j][k]);
                 }
             }
+            // printf("task %d \n", i);
         }
         int ans = 0;
-        for(int i = 0; i < sz; i++)
+        
+        for(int k = 0; k < G + 1; k++)
         {
-            for(int j = 0; j < G + 1; j++)
-            {
-                ans += dp[i][P][j] % MOD;
-            }
+            ans += (dp[sz][P][k] % MOD);
+            // printf("ans is now %d \n", ans);
         }
+        
         return ans;
 
     }
+    
 };
