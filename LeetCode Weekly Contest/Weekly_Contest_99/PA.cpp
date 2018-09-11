3dce0644b5b79911de5d7881645e658989cabcf2
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2018-09-12 00:40:25 +0800

    Doing WC9899100 now.

--- LeetCode Weekly Contest/Weekly_Contest_99/PA.cpp
+++ LeetCode Weekly Contest/Weekly_Contest_99/PA.cpp
@@ -1,53 +1,14 @@
-class Solution
-{
+class Solution {
 public:
-    vector<int> fairCandySwap(vector<int>& A, vector<int>& B)
-    {
-        int suma, sumb, flg;
-        suma = sumb = 0;
-        flg = 0;
-        vector<int> res;
-        for(int i = 0; i < A.size(); i++)
-        {
-            suma += A[i];
-        }
-        for(int i = 0; i < B.size(); i++)
-        {
-            sumb += B[i];
-        }
-        int fair = (suma + sumb) / 2;
-        for(int i = 0; i < A.size(); i++)
-        {
-            for(int j = 0; j < B.size(); j++)
-            {
-                if(suma - A[i] + B[j] == sumb - B[j] + A[i])
-                {
-                    res.push_back(A[i]);
-                    res.push_back(B[j]);
-                    flg = 1;
-                    break;
-                }
-            }
-            if(flg)
-            {
-                break;
+    int surfaceArea(vector<vector<int>> grid) {
+        int res = 0, n = grid.size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j]) res += grid[i][j] * 4 + 2;
+                if (i) res -= min(grid[i][j], grid[i - 1][j]) * 2;
+                if (j) res -= min(grid[i][j], grid[i][j - 1]) * 2;
             }
         }
         return res;
-    }
-    bool check(vector<int>& A, vector<int>& B)
-    {
-        int suma, sumb;
-        suma = sumb = 0;
-        for(int i = 0; i < A.size(); i++)
-        {
-            suma += A[i];
-        }
-        for(int i = 0; i < B.size(); i++)
-        {
-            sumb += B[i];
-        }
-        //printf("suma %d, sumb %d \n", suma, sumb);
-        return (suma == sumb) && (suma) && (sumb);
     }
 };
