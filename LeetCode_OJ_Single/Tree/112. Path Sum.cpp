9c8a2e7bc7e8d630e062ef207e1a5d8abfeb35a2
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-02-25 22:31:01 +0800

    Retry LeetCode 112 and 113, making code clean

--- LeetCode_OJ_Single/Tree/112. Path Sum.cpp
+++ LeetCode_OJ_Single/Tree/112. Path Sum.cpp
@@ -1,48 +1,16 @@
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
- */
-class Solution
+class Solution 
 {
 public:
-    bool flg = false;
-    int cur_sum = 0;
-    bool hasPathSum(TreeNode* root, int sum)
-    {
-        return descend(root, sum);
-    }
-    bool descend(TreeNode* root, int& target)
+    bool hasPathSum(TreeNode* root, int sum) 
     {
         if(root == NULL)
         {
-            return false;
+            return false; 
         }
-        cur_sum += root->val;
-            
-        if(cur_sum == target && is_leaf(root))
+        if(root != NULL && root ->left == NULL && root -> right == NULL)
         {
-            return true;
+            return root -> val == sum;
         }
-        else if(cur_sum != target && is_leaf(root))
-        {
-            cur_sum -= root->val;
-            return false;
-        }
-        bool left_can = descend(root->left, target);
-        bool right_can = descend(root->right, target);
-        if(!left_can && !right_can)
-        {
-            cur_sum -= root->val;
-        }
-        return left_can | right_can;
-    }
-    bool is_leaf(TreeNode* root)
-    {
-        return root->left == NULL && root->right == NULL;
+        return hasPathSum(root -> left, sum - root->val) || hasPathSum(root -> right, sum - root -> val);
     }
 };
