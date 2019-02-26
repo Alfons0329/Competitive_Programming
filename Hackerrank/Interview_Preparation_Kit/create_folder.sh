0c8f2619654e37d93e651f730eee57be4f645eea
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-02-26 22:20:01 +0800

    Practice for tech interview during the holidays

---
+++ Hackerrank/Interview_Preparation_Kit/create_folder.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+for i in Array Tips_and_Guidelines Dictionaries_and_Hashmaps Sorting String_Manipulation Greedy_Algorithm Search DP Stack_and_Queue Graph Tree Linked_List Recursion_and_Backtracking Miscellaneous;
+do
+    mkdir -p $i
+done
