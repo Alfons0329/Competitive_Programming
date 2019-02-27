11dd1dd2c7f7bbb072de7114553d7790e09450f7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-02-27 09:33:16 +0800

    Doing more problems

--- Hackerrank/Interview_Preparation_Kit/Dictionaries_and_Hashmaps/sherlok.cpp
+++ Hackerrank/Interview_Preparation_Kit/Dictionaries_and_Hashmaps/sherlok.cpp
@@ -5,8 +5,42 @@ using namespace std;
 // Complete the sherlockAndAnagrams function below. ie how many pairs of substring anagrams??
 int sherlockAndAnagrams(string s) 
 {
+    int sz = s.size();
+    
+    for(int i = 1; i < sz - 1; i++) // brute force finding the substring pairds starting from len 1 to len size - 1 
+    {
 
 
+    
+
+    }
+}
+bool check(string s1, string s2)
+{
+    unordered_map<char, int> mp1;
+    unordered_map<char, int> mp2;
+
+    int n = s1.size(), m = str2.size();
+    if(n != m)
+    {
+        return false;
+    }
+
+    for(int i = 0; i < s1.size(); i++)
+    {
+        mp1[s1[i]]++;
+        mp2[s2[i]]++;
+    }
+
+    //check map composition
+    for(auto& it : mp1)
+    {
+        if(mp2.count(it))
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main()
