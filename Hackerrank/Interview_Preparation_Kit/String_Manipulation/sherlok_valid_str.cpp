f99779f7eadcef49cceb75f22d36e74353a53740
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-02-28 12:56:45 +0800

    AC Sherlok and the valid string

--- Hackerrank/Interview_Preparation_Kit/String_Manipulation/sherlok_valid_str.cpp
+++ Hackerrank/Interview_Preparation_Kit/String_Manipulation/sherlok_valid_str.cpp
@@ -13,28 +13,47 @@ string isValid(string s)
     {
         map_1[s[i]]++;
     }
-
-    cout << "kk" << endl;
-    unordered_map<int, int> map_2; // stat data for occurance of character occurance
-    for(unordered_map<char, int>:: iterator it = map_1.begin(); it != map_1.end(); it++)
+    
+    map<int, int> freq_cnt; // order matters
+    for(unordered_map<char, int>::iterator it = map_1.begin() ;it != map_1.end(); it++)
     {
-        map_2[it -> second]++;
+        // cout << it -> first << " , " << it -> second << endl;
+        freq_cnt[it -> second]++;
+    }
+    if(freq_cnt.size() > 2)
+    {
+        return "NO";
+    }
+    else if(freq_cnt.size() == 1) // all the same frequency, ex aabbccddee, we only have 2 : 5
+    {
+        return "YES";
     }
 
-    cout << "kk" << endl;
-    if(map_2.size() > 2)
-    {
-        res = "NO";
-        return res;
-    }
+    map<int, int>::iterator it = freq_cnt.begin();
+    int f1 = it -> first;
+    int lower_freq_cnt = it -> second; 
 
-    cout << "kk" << endl;
-    // compare the remain 2 elemet
-    unordered_map<int, int>:: iterator it1 = map_2.begin();
-    unordered_map<int, int>:: iterator it2 = map_2.end();
-    cout << "it1sec " << it1 -> second << "  it2sec   " << it2 -> second << endl;
-    res = (abs(it1 -> second - it2 -> second) > 1) ? "NO" : "YES";
-    return res;
+    map<int, int>::iterator it2 = ++it;
+    int f2 = it2 -> first;
+    int higher_freq_cnt = it2 -> second;
+    
+    int lower_freq = min(f1, f2), higher_freq = max(f1, f2);
+    printf("lfq %d, lfqcnt %d, hfq %d, hfqcnt %d\n", lower_freq, lower_freq_cnt, higher_freq, higher_freq_cnt); 
+
+    if(lower_freq == 1 && lower_freq_cnt == 1)
+    {
+        return "YES";
+    }
+    else if(higher_freq - lower_freq == 1 && higher_freq_cnt == 1) // remove the only 1 freq to satisfy thr string such as aaaab 4 : 1 and 1 : 1 or aabbbcc 2 : 2 3 : 1
+    {
+        return "YES";
+    }
+    else if(higher_freq - lower_freq > 1) // things like aabbcccc 2 : 2 and 4 : 1 where, lower_freq = 2, higher_freq = 4 should remove 2
+    {
+        return "NO";
+    }
+    
+    return "NO";
 }
 
 int main()
@@ -45,7 +64,6 @@ int main()
     getline(cin, s);
 
     string result = isValid(s);
-
     fout << result << "\n";
 
     fout.close();
