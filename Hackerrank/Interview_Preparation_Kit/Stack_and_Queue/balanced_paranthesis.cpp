068cce733513720ec016e3ae021e1540df3d1028
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-04 17:04:40 +0800

    AC Balanced brackets in stack and queues

---
+++ Hackerrank/Interview_Preparation_Kit/Stack_and_Queue/balanced_paranthesis.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the isBalanced function below.
+string isBalanced(string s) 
+{
+    stack<char> stk;
+    int n = s.size();
+    for(int i = 0; i < n; i++)
+    {
+        if(s[i] == '(' || s[i] == '{' ||s[i] == '[' )
+        {
+            stk.push(s[i]);
+        }
+        else if(s[i] == ')')
+        {
+            if(stk.empty())
+            {
+                return "NO";
+            }
+            if(stk.top() == '(')
+            {
+                stk.pop();
+            }
+            else
+            {
+                return "NO";
+            }
+        }
+        else if(s[i] == '}')
+        {
+            if(stk.empty())
+            {
+                return "NO";
+            }
+            if(stk.top() == '{')
+            {
+                stk.pop();
+            } 
+            else
+            {
+                return "NO";
+            }
+        }
+        else if(s[i] == ']')
+        {
+            if(stk.empty())
+            {
+                return "NO";
+            }
+            if(stk.top() == '[')
+            {
+                stk.pop();
+            } 
+            else
+            {
+                return "NO";
+            }
+        }
+
+        
+    }
+
+    if(stk.empty() != true)
+    {
+        return "NO";
+    }
+    return "YES";
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int t;
+    cin >> t;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int t_itr = 0; t_itr < t; t_itr++) {
+        string s;
+        getline(cin, s);
+
+        string result = isBalanced(s);
+
+        fout << result << "\n";
+    }
+
+    fout.close();
+
+    return 0;
+}
+
