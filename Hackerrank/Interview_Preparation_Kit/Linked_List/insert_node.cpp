d3ce54552e816beb21fb52639cb913875209fbb7
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-03-05 22:46:51 +0800

    AC Some LL problems

---
+++ Hackerrank/Interview_Preparation_Kit/Linked_List/insert_node.cpp
@@ -0,0 +1,152 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/*void traverse(SinglyLinkedListNode* head)
+{
+    printf("LL: ");
+    while(head != NULL)
+    {
+       printf("%d ", head -> data);
+       head = head -> next;
+    }
+    printf("\n");
+}*/
+
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) 
+{
+    
+    SinglyLinkedListNode* new_node = new SinglyLinkedListNode(data); 
+
+    if(position == 0) // insert the node at the head
+    {
+        new_node -> next = head;
+        head = new_node;//adjust head
+        return head;
+    }
+
+    // traverse the node before the node to be inserted and insertion
+    int pos_cnt = 0;
+    SinglyLinkedListNode* cur_node = head;
+    while(cur_node != NULL)
+    {
+        if(pos_cnt == position - 1) // before that node
+        {
+            break;
+        }
+        cur_node = cur_node -> next;
+        pos_cnt++;
+    }
+    
+    // insertion process and rearrange
+    
+    new_node -> next = cur_node -> next -> next;
+    cur_node -> next = new_node;
+    // traverse(head);
+    return head;
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    SinglyLinkedList* llist = new SinglyLinkedList();
+
+    int llist_count;
+    cin >> llist_count;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int i = 0; i < llist_count; i++) {
+        int llist_item;
+        cin >> llist_item;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        llist->insert_node(llist_item);
+    }
+
+    int data;
+    cin >> data;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int position;
+    cin >> position;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    SinglyLinkedListNode* llist_head = insertNodeAtPosition(llist->head, data, position);
+
+    print_singly_linked_list(llist_head, " ", fout);
+    fout << "\n";
+
+    free_singly_linked_list(llist_head);
+
+    fout.close();
+
+    return 0;
+}
+
