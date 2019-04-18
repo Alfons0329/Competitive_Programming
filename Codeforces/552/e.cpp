0dd351a5b135ede8582a6031090dcf881d92562e
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-04-18 15:10:31 +0800

    Doing CF 552, E, code not good

--- Codeforces/552/e.cpp
+++ Codeforces/552/e.cpp
@@ -36,11 +36,72 @@ const ll P = 92540646808111039LL;
 const ll maxn = 1e5 + 10, MOD = 1e9 + 7;
 const int Move[4][2] = {-1,0,1,0,0,1,0,-1};
 const int Move_[8][2] = {-1,-1,-1,0,-1,1,0,-1,0,1,1,-1,1,0,1,1};
-
+int cmp(int a, int b)
+{
+    return a > b;
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     
+    int n, k, tmp, cnt = 0;
+    vi v, v2;
+    mii ith_pos;
+
+    cin >> n >> k;
+    while(n--)
+    {
+        cin >> tmp;
+        v.pb(tmp);
+        ith_pos[tmp] = cnt++;
+    }
+    v2 = v;
+    sort(v2.begin(), v2.end(), cmp);
+
+    int teacher = 0, taken = 0;
+    n = v2.size();
+
+    for(int i = 0; i < n; ++i)
+    {
+        cnt = 1;
+        if(taken >= n)
+        {
+            break;
+        }
+        printf("ake vpos %d to %d \n", ith_pos[v2[i]], teacher);
+        if(v[ith_pos[v2[i]]] > 0)
+        {
+            v[ith_pos[v2[i]]] = (teacher == 0) ? -1 : -2;
+            v2.erase(find(v[ith_pos[v2[i]]]));
+            ++taken;
+        }
+        while(cnt <= k)
+        {
+            if(v[(ith_pos[v2[i]] + cnt) % n] >  0)
+            {
+                printf("make vpos %d v %d to %d \n", (ith_pos[v2[i]] + cnt) % n, v[(ith_pos[v2[i]] + cnt) % n], teacher);
+                v[ith_pos[v2[i]] + cnt] = (teacher == 0) ? -1 : -2;
+                ++taken;
+            }
+
+            if(ith_pos[v2[i]] - cnt >= 0 && v[ith_pos[v2[i]] - cnt] >  0)
+            {
+                printf("Make vpos %d v %d to %d \n", ith_pos[v2[i]] - cnt, v[ith_pos[v2[i]] - cnt], teacher);
+                v2.erase(find(v[ith_pos[v2[i]] - cnt]));
+                v[ith_pos[v2[i]] - cnt] = (teacher == 0) ? -1 : -2;
+                ++taken;
+            }
+            ++cnt;
+        }
+        teacher ^= 1;
+        printf("change teacher\n");
+    }
+
+    wz;
+    for(auto i : v)
+    {
+        cout << abs(i) << ' ';
+    }
     return 0;
 }
