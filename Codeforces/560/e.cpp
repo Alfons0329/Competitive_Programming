1470b788fb055b34445c0d60d3a52528c6e7f2a1
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-16 22:37:05 +0800

    CF560 A~E all AC

--- Codeforces/560/e.cpp
+++ Codeforces/560/e.cpp
@@ -31,16 +31,64 @@ using namespace std;
 const int INF_INT = 2147483647;
 const ll INF_LL = 9223372036854775807LL;
 const ull INF_ULL = 18446744073709551615Ull;
-const ll P = 92540646808111039LL;
 
-const ll maxn = 1e5 + 10, MOD = 1e9 + 7;
-const int Move[4][2] = {-1,0,1,0,0,1,0,-1};
-const int Move_[8][2] = {-1,-1,-1,0,-1,1,0,-1,0,1,1,-1,1,0,1,1};
+const ull mod =  998244353;
+
+struct ele
+{
+    ll v;
+    ll con; // how much contribution to the result
+    ll pos;
+};
+bool cmp(ele a, ele b)
+{
+    return a.con < b.con;
+}
 
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
+
+    int n;
+    ll tmp;
+
+    vector<ele> a;
+    vll b;
+
+    cin >> n;
+    ele e;
+    e.v = 0;
+    e.pos = 0;
+    a.pb(e);
+
+    b.pb(INF_LL);
+
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> tmp;
+        e.v = tmp;
+        e.pos = i;
+        e.con =  i * (n - i + 1ll) * tmp;
+        a.pb(e);
+    }
+    for(int i = 1; i <= n; ++i)
+    {
+        cin >> tmp;
+        b.pb(tmp);
+    }
+
+    sort(a.begin() + 1, a.end(), cmp);
+    sort(b.begin() + 1, b.end());
+    reverse(b.begin() + 1, b.end());
     
+    ll res = 0;
+    for(int i = 1; i <= n; ++i)
+    {
+        res = res % mod + (a[i].con % mod) * (b[i] % mod) % mod;
+        // printf("i %d j %d k %d ak %llu bk %llu res %llu\n", i, j, k, a_bk[k].v, b_bk[k], res);
+    }
+
+    cout << res % mod;
     return 0;
 }
