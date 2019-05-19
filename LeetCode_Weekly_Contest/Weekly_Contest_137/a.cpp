a6c661a9f5908b4c48c661584719f475d3b99024
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-19 15:45:55 +0800

    AC WC137/C with DP

--- LeetCode_Weekly_Contest/Weekly_Contest_137/a.cpp
+++ LeetCode_Weekly_Contest/Weekly_Contest_137/a.cpp
@@ -3,25 +3,22 @@ class Solution
 public:
     int lastStoneWeight(vector<int>& stones) 
     {
-        int n = stones.size();
         while(true)
         {
             sort(stones.begin(), stones.end());
-            n = stones.size();
-            
+            int n = stones.size();
+
             if(n <= 1)
             {
                 break;
             }
             
-            int a = stones[n - 1];
-            int b = stones[n - 2];
-            
-            int x = min(a, b);
-            int y = max(a, b);
+            int y = stones[n - 1];
+            int x = stones[n - 2];
             
             stones.pop_back();
             stones.pop_back();
+
             if(x != y)
             {
                 stones.push_back(y - x);
