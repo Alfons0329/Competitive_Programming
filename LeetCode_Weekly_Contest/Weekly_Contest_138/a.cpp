79c601ad36892e978cc3c239f2d8af74129e105e
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-05-26 15:39:42 +0800

    AC WC 138 A C D

---
+++ LeetCode_Weekly_Contest/Weekly_Contest_138/a.cpp
@@ -0,0 +1,29 @@
+class Solution 
+{
+    public:
+        static bool cmp(int a, int b)
+        {
+            return a <= b;
+
+        }
+        int heightChecker(vector<int>& heights) 
+        {
+            int n = heights.size(), res = 0;
+            vector<int> h2 = heights;
+            sort(h2.begin(), h2.end());
+
+            for(int i = 0; i < n; i++)
+            {
+                if(heights[i] != h2[i])
+                {
+                    res++;
+
+                }
+
+            }
+            return res;
+
+
+        }
+
+};
---
+++ LeetCode_Weekly_Contest/Weekly_Contest_138/c.cpp
@@ -0,0 +1,47 @@
+class Solution {
+    public:
+        bool check(vector<int>& A)
+        {
+            int n = A.size();
+            for(int i = 0; i < n - 1; i++)
+            {
+                if(A[i] > A[i + 1])
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        vector<int> prevPermOpt1(vector<int>& A) 
+        {
+            if(check(A))
+            {
+                return A;
+            }
+
+            int n = A.size(), pos = 0, maxval = 0;
+
+            for(int i = 0; i < n - 1; i++)
+            {
+                if(A[i] > A[i + 1])
+                {
+                    pos = i;
+                }
+            }
+
+            int swappos = 0, smallese = INT_MAX;
+            for(int i = pos + 1; i < n; i++)
+            {
+                if(A[i] < A[pos] && A[i] != A[i - 1])
+                {
+                    swappos = i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            swap(A[pos], A[swappos]);
+            return A;
+        }
+};
