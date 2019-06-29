95b531467a51cc095920ceb28dfcc3ad631401a8
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-06-29 23:20:38 +0800

    AC problem A-C during contest, working on D

--- Codeforces/570/a.cpp
+++ Codeforces/570/a.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+ 
 #define ll long long
 #define ull unsigned long long
 #define PI acos(-1.0)
@@ -12,46 +12,60 @@ using namespace std;
 #define wz cout<<"-----"<<endl;
 #define pb push_back
 #define mp make_pair
-
+ 
 #define vs vector<string> 
 #define vi vector<int> 
 #define vll vector<ll> 
 #define vull vector<ull>
-
+ 
 #define pii pair<int,int>
-
+ 
 #define msi map<string, int>
 #define mci map<char, int>
 #define mii map<int, int>
-
+ 
 #define usi unordered_map<string, int>
 #define uci unordered_map<char, int>
 #define uii unordered_map<int, int>
-
+ 
 const int INF_INT = 2147483647;
 const ll INF_LL = 9223372036854775807LL;
 const ull INF_ULL = 18446744073709551615Ull;
 const ll P = 92540646808111039LL;
-
+ 
 const ll maxn = 1e5 + 10, MOD = 1e9 + 7;
 const int Move[4][2] = {-1,0,1,0,0,1,0,-1};
 const int Move_[8][2] = {-1,-1,-1,0,-1,1,0,-1,0,1,1,-1,1,0,1,1};
-
+int dig_sum(int n)
+{
+    int res = 0;
+    while(n)
+    {
+        res += n % 10;
+        n /= 10;
+    }
+    return res;
+}
 int main()
 {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    int T;
-    cin >> T;
-    while(T--)
+    int n;
+    cin >> n;
+    if(dig_sum(n) % 4 == 0)
     {
-        int n;
-        vi v(n);
-        for(int i = 0; i < n; i++)
+        cout << n;
+    }
+    else
+    {
+        while(n++)
         {
-            cin >> v[i];
+            if(dig_sum(n) % 4 == 0)
+            {
+                break;
+            }
         }
-
+        cout << n;
     }
     
     return 0;
