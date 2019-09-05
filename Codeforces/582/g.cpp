aad6fb843fd1a3550b2a219bb7531513f9d82710
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2019-09-06 00:08:05 +0800

    Doing 582 E

---
+++ Codeforces/582/g.cpp
@@ -0,0 +1,78 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ull unsigned long long
+#define PI acos(-1.0)
+#define eps 1e-12
+#define fi first
+#define se second
+#define MEM(a,b) memset((a),(b),sizeof(a))
+#define mod(x) ((x)%MOD)
+#define wz cout<<"-----"<<endl;
+#define pb push_back
+#define mp make_pair
+
+#define vs vector<string> 
+#define vi vector<int> 
+#define vll vector<ll> 
+#define vull vector<ull>
+
+#define pii pair<int,int>
+
+#define msi map<string, int>
+#define mci map<char, int>
+#define mii map<int, int>
+
+#define usi unordered_map<string, int>
+#define uci unordered_map<char, int>
+#define uii unordered_map<int, int>
+
+const int INF_INT = 2147483647;
+const ll INF_LL = 9223372036854775807LL;
+const ull INF_ULL = 18446744073709551615Ull;
+const ll P = 92540646808111039LL;
+
+const ll maxn = 1e5 + 10, MOD = 1e9 + 7;
+const int Move[4][2] = {-1,0,1,0,0,1,0,-1};
+const int Move_[8][2] = {-1,-1,-1,0,-1,1,0,-1,0,1,1,-1,1,0,1,1};
+
+vector<pair<int, pii> > e; // (weight, (u, v) edge)
+ll tmp_res;
+
+ll comb(int n)
+{
+    return n * 1LL * (n - 1) / 2;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+
+    // IO
+    int n, m;
+    cin >> n >> m;
+
+    vector<pii> q(m); // store queries
+    vector<ll> res(m);
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        cin >> e[i].second.first >> e[i].second.second >> e[i].first;
+        e[i].second.first--;
+        e[i].second.second--;
+    }
+
+    for (int i = 0; i < m; ++i) 
+    {
+        cin >> q[i].first;
+        q[i].second = i;
+    }
+    sort(e.begin(), e.end()); // sort edges based on weight
+    sort(q.begin(), q.end()); // sort queries based on weight
+
+    // solve
+
+    return 0;
+}
