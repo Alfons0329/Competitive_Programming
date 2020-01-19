9eaf0ceaa4d11fa87a59709e3d76d6d8043bce7b
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-01-19 12:51:32 +0800

    LeetCode WC172 AC problem A, B and C

--- LeetCode_Weekly_Contest/Weekly_Contest_172/c.py
+++ LeetCode_Weekly_Contest/Weekly_Contest_172/c.py
@@ -1,24 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 class Solution:
     def removeLeafNodes(self, root: TreeNode, target: int) -> TreeNode:
-        root = self.dfs(root, target)
-        return root
-    
-    def dfs(self, root: TreeNode, target: int):
         if root == None:
             return None 
         
         # connect, to check child existence
-        root.left = self.dfs(root.left, target)
-        root.right = self.dfs(root.right, target)
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
 
-        # self destruction
         if root.val == target and root.left == None and root.right == None:
                 return None
         return root
