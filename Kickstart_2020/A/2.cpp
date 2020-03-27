f597ead8e47a702e4501941f43ebf4a4af3638c6
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-03-27 09:59:45 +0800

    AC PLates

--- Kickstart_2020/A/2.cpp
+++ Kickstart_2020/A/2.cpp
@@ -1,6 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[55][1505];
+int a[55][35];
+int pfx[55][1505];
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
@@ -9,24 +12,27 @@ int main(){
     cin >> T;
     while (T--) {
         cin >> n >> k >> p;
-        int dp[55][1505] = {0};                            // answer
-        vector<vector<int>> pfx(55, vector<int>(1505, 0)); // prefix sum
 
-        for (int i = 0; i < n; i++){
+        memset(dp, 0, sizeof(dp));
+        memset(a, 0, sizeof(a));
+        memset(pfx, 0, sizeof(pfx));
+        
+        for (int i = 1; i <= n; i++){
             for (int j = 1; j <= k; j++){
-                cin >> tmp;
-                pfx[i][j] = pfx[i][j - 1] + tmp;
+                cin >> a[i][j];
+                pfx[i][j] = pfx[i][j - 1] + a[i][j];
             }
         }
-
-        for (int i = 0; i < n; i++){
+        
+        for (int i = 1; i <= n; i++){
             for (int j = 1; j <= k; j++){
                 for (int l = 0; l + j <= p; l++){
                     dp[i][l + j] = max(dp[i][l + j], max(dp[i - 1][l + j], dp[i - 1][l] + pfx[i][j]));
                 }
             }
         }
-        cout << "Case #" << cnt++ << ": " << dp[n - 1][p] << '\n';
+
+        cout << "Case #" << cnt++ << ": " << dp[n][p] << '\n';
     }
 
     return 0;
