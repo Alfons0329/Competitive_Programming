a91c4314a154d1096a250549a0d66b353fbbc77c
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-04-20 21:38:15 +0800

    Day 19, Update for real O(log(N))

--- LeetCode_30Days_Challenge/0419.cpp
+++ LeetCode_30Days_Challenge/0419.cpp
@@ -6,11 +6,20 @@ public:
             int m = l + (r - l) / 2;
             if (nums[m] == target) return m;
             if (nums[m] < nums[r]) {
-                if (nums[m] < target && nums[r] >= target) l = m + 1;
-                else r = m - 1;
-            } else {
-                if (nums[l] <= target && nums[m] > target) r = m - 1;
-                else l = m + 1;
+                if (nums[m] < target && nums[r] >= target){
+                    l = m + 1;
+                }
+                else{
+                    r = m - 1;
+                }
+            } 
+            else {
+                if (nums[l] <= target && nums[m] > target){
+                    r = m - 1;
+                } 
+                else{
+                    l = m + 1;
+                }
             }
         }
         return -1;
