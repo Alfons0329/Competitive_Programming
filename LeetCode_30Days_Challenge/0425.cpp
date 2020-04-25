0cda661563f1518e885ef23c61d9dec1e95dd154
Alfons Hwu <alfons.cs04@g2.nctu.edu.tw>
2020-04-25 17:02:30 +0800

    Create 0425.cpp

---
+++ LeetCode_30Days_Challenge/0425.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int max_pos = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(i <= max_pos){
+                max_pos = max(max_pos, i + nums[i]);
+            }
+        }
+        return max_pos >= nums.size() - 1;
+    }
+};
