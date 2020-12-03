179102c410fc45c52362cbd7ff99093e1f23cc01
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-12-03 12:10:07 +0800

    Shell script for lazy person

---
+++ CTCI/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+
+p=$1
+
+if [ -e $p.c ]
+then
+    gcc -std=c99 $p.c && ./a.out < $p\_in.txt
+elif [ -e $p.cpp ]
+then
+    g++ -std=c+=14 $p.cpp && ./a.out < $p\_in.txt
+else
+    exit
+fi
