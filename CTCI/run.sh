b39265ceb4a8a522d22cd49f10b4a11ed90cf2fb
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-12-03 18:49:23 +0800

    Shell script for lazy person, updated

--- CTCI/run.sh
+++ CTCI/run.sh
@@ -5,9 +5,21 @@ p=$1
 if [ -e $p.c ]
 then
     gcc -std=c99 $p.c && ./a.out < $p\_in.txt
+    if [ -e $p\_in.txt ]
+    then
+        g++ -std=c99 $p.c && ./a.out < $p\_in.txt
+    else
+        g++ -std=c99 $p.c && ./a.out
+    fi
+
 elif [ -e $p.cpp ]
 then
-    g++ -std=c+=14 $p.cpp && ./a.out < $p\_in.txt
+    if [ -e $p\_in.txt ]
+    then
+        g++ -std=c++14 $p.cpp && ./a.out < $p\_in.txt
+    else
+        g++ -std=c++14 $p.cpp && ./a.out
+    fi
 else
     exit
 fi
