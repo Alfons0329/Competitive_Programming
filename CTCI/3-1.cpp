fba61e20ef4bc4543d1c069d78a9eabec47ba0ff
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-12-04 10:57:33 +0800

    CTCI practice in pure C, Ch 3-1

--- CTCI/3-1.cpp
+++ CTCI/3-1.cpp
@@ -14,7 +14,7 @@ public:
     mystack(){
         printf("Constructor\n");
         stk = (int*) calloc(0, sizeof(int) * STACK_CNT * STACK_SIZE);
-        top_idx[0] = top_idx[1] = top_idx[2] = 0;
+        offset[0] = offset[1] = offset[2] = 0;
     }
 
     virtual ~mystack(){
@@ -22,47 +22,53 @@ public:
         free(stk);
     }
 
-    int push(int stack_id, int val){
-        assert(top_idx[stack_id] < STACK_SIZE);
-        stk[top_idx[stack_id]] = val;
-        top_idx[stack_id]++;
+    void push(int stack_id, int val){
+        assert(offset[stack_id] < STACK_SIZE);
+        stk[stack_id * STACK_SIZE + offset[stack_id]] = val;
+        offset[stack_id]++;
     }
 
-    int pop(int stack_id){
-        assert(top_idx[stack_id] >= 0);
-        int ret = stk[top_idx[stack_id]];
-        top_idx[stack_id]--;
-        return ret;
+    void pop(int stack_id){
+        assert(offset[stack_id] > 0);
+        offset[stack_id]--;
     }
 
     bool empty(int stack_id){
-        return top_idx[stack_id] == 0;
+        return offset[stack_id] == 0;
     }
 
     int top(int stack_id){
-        assert(top_idx[stack_id] >= 0);
-        int ret = stk[top_idx[stack_id]];
-        return ret;
+        assert(offset[stack_id] <= STACK_SIZE);
+        return stk[stack_id * STACK_SIZE + offset[stack_id] - 1];
     }
 
 
 private:
     int* stk;
-    int top_idx[STACK_CNT];
+    int offset[STACK_CNT];
 }mystack;
 
 int main(){
 
     mystack my;
-    my.push(0, 1);
-    my.push(0, 2);
-    my.push(0, 3);
-    my.push(1, 1);
-    my.push(1, 1);
-    my.push(1, 1);
-    my.push(2, 3);
-    my.push(2, 3);
-    my.push(2, 3);
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++){
+            my.push(i, i * 5 + j);
+            printf("Push stack %d, top %d \n", i, my.top(i));
+        }
+    }
+
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 5; j++){
+            printf("Pop stack %d, top %d \n", i, my.top(i));
+            my.pop(i);
+        }
+        printf("Stack %d is empty %d \n", i, my.empty(i));
+    }
+
+    // Assertion test
+    printf("Final \n");
+    my.pop(2);
 
     return 0;
 } 
--- CTCI/run.sh
+++ CTCI/run.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xe
+set -e
 p=$1
 
 if [ -e $p.c ]
