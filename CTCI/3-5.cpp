6ea11bcad86c1b0c116e6d8cf1ed22ee36720b16
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-12-04 16:09:48 +0800

    CTCI 3-5, from LeetCode problem ID 232

---
+++ CTCI/3-5.cpp
@@ -0,0 +1,37 @@
+class MyQueue {
+public:
+    
+    stack<int> in, out;
+    /** Initialize your data structure here. */
+    MyQueue() {
+        
+    }
+    
+    /** Push element x to the back of queue. */
+    void push(int x) {
+        in.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    int pop() {
+        int ret = peek();
+        out.pop();
+        return ret;
+    }
+    
+    /** Get the front element. */
+    int peek() {
+        if(out.empty()){
+            while(in.size()){
+                out.push(in.top());
+                in.pop();
+            }
+        }
+        return out.top();
+    }
+    
+    /** Returns whether the queue is empty. */
+    bool empty() {
+        return in.empty() && out.empty();
+    }
+};
