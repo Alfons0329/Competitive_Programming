4d5b32d7874ae409367ffab060f1c6ec06dd3497
Alfons0329 <alfons.cs04@g2.nctu.edu.tw>
2020-12-05 16:51:13 +0800

    CTCI practice in C-Style, Ch 5, ongoing

---
+++ CTCI/5-4.cpp
@@ -0,0 +1,19 @@
+/*
+ * Explain what the following code does: ((n & (n-1)) == 0). 
+ * */
+#include <bits/stdc++.h>
+
+bool check_pow_2(unsigned long num){
+    return (num & (num - 1)) == 0;
+}
+
+using namespace std;
+
+int main(){
+    for(unsigned long i = 0; i < 1ULL << 31; i += 2){
+        if(check_pow_2(i)){
+            cout << i << " is pow of 2 " << endl;
+        }
+    }
+    return 0;
+}
